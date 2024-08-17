6e82960c264299b9cf4d497dfcd0d07d92a1c8ba
Ollie <oliver2.thomson@live.uwe.ac.uk>
2024-08-17 16:53:59 +0100

    Added basic sawblade

--- Assets/Scripts/Hazards/SawBlade.cs
+++ Assets/Scripts/Hazards/SawBlade.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spike : MonoBehaviour
+public class SawBlade : MonoBehaviour
 {
     private float damage = 10;
+    private Vector3 rotation = new Vector3(0, 0, 20);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles += rotation;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
