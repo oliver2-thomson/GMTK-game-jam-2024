2b4beee7ad7d85646da89a3e874db8db4b3d237e
volx64 <115743584+volx64@users.noreply.github.com>
2024-08-17 13:48:46 +0100

    Push (#11)
    * Removed ReadMe
    
    * Added 2D packages
    
    * BasedBlocks
    
    * BaseClass / Attachments mostly done
    
    ---------
    
    Co-authored-by: oliver2-thomson <146836358+oliver2-thomson@users.noreply.github.com>

--- Assets/Scripts/Blocks/BaseBlock.cs
+++ Assets/Scripts/Blocks/BaseBlock.cs
@@ -4,6 +4,50 @@ using UnityEngine;
 
 public class BaseBlock : MonoBehaviour
 {
+    [System.Flags]
+    public enum FaceType
+    {
+        Top = 0, 
+        Right = 1, 
+        Bottom = 2, 
+        Left = 3
+    }
+
+    [EnumFlagsAttributes]
+    private FaceType enumType;
+
+
+    public List<int> ReturnAllFaceElements()
+    {
+        List<int> selectedElements = new List<int>();
+        for (int i = 0; i < System.Enum.GetValues(typeof(FaceType)).Length; i++)
+        {
+            int layer = 1 << i;
+            if (((int)enumType & layer) != 0)
+            {
+                selectedElements.Add(i);
+            }
+        }
+
+        return selectedElements;
+    }
+
+    public void OnPlacedTile() 
+    {
+        //Todo parse oriational data to block
+    }
+
+
+    /// <summary>
+    /// Use this to Check what face is an attachable point on a block
+    /// </summary>
+    /// <param name="face"></param>
+    /// <returns></returns>
+    public bool CheckFace(FaceType face) 
+    {
+        List<int> faceList = ReturnAllFaceElements();
+
+        return faceList.Contains((int)face);
     private bool[,] blockSlots = new bool[9, 9];
     
     public int BlockWidth
