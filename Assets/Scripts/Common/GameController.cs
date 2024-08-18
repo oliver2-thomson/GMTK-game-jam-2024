185208d7cbe70dd3d9e1290721956dcf4cfec1dd
Dominik Heiler <dominik2.heiler@live.uwe.ac.uk>
2024-08-18 22:43:35 +0100

    Block editing mode when the player holds down 'Space'

--- Assets/Scripts/Common/GameController.cs
+++ Assets/Scripts/Common/GameController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class GameController : MonoBehaviour
 {
@@ -11,20 +12,46 @@ public class GameController : MonoBehaviour
     // Basic settings
     public bool IsEditingBlocks;
     public bool IsPaused;
+    [Space(10)]
+    public Camera Camera;
+    public PlayerAttachment Player;
 
     // Private variables
     private UIPauseScreen pauseUI;
+    private UniversalAdditionalCameraData cameraData;
 
     private void Awake()
     {
         instance = this;
 
         pauseUI = FindObjectOfType<UIPauseScreen>();
+        cameraData = Camera.GetUniversalAdditionalCameraData();
     }
 
     private void Update()
     {
         // Switching between editing and playing modes
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            IsEditingBlocks = true;
+            foreach (Camera newCam in cameraData.cameraStack)
+            {
+                newCam.enabled = true;
+            }
+
+            Player.ShowAttachmentUI();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            IsEditingBlocks = false;
+            foreach (Camera newCam in cameraData.cameraStack)
+            {
+                newCam.enabled = false;
+            }
+
+            Player.HideUI();
+        }
         
         // Pausing
         if (Input.GetKeyDown(KeyCode.Return))
