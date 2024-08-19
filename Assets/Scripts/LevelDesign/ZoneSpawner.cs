b8d376a5db0fcb67ac3f67c42eafe083631e3593
volx64 <115743584+volx64@users.noreply.github.com>
2024-08-20 00:34:37 +0100

    Added Zone Spawner

--- Assets/Scripts/LevelDesign/ZoneSpawner.cs
+++ Assets/Scripts/LevelDesign/ZoneSpawner.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class ZoneSpawner : ZoneItem
 {
+    [Space]
+    [Header("Drag enemy from Prefab/AI to Below!")]
+    [SerializeField] Enemy enemy;
+
+    bool hasSpawned = false;
     public override void OnZoneEnter()
     {
-        
+        if (!hasSpawned) 
+        {
+            GameObject.Instantiate(enemy, transform.position, transform.rotation, null);
+            hasSpawned = true;
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (enemy != null)
+        {
+            Gizmos.DrawIcon(transform.position, enemy.gameObject.name);
+        }
     }
 }
