d8e7fc21dbdde4a630b7dcd043a0c558263ad447
volx64 <115743584+volx64@users.noreply.github.com>
2024-08-20 18:03:00 +0100

    Update ZoneMusic.cs

--- Assets/Scripts/LevelDesign/ZoneMusic.cs
+++ Assets/Scripts/LevelDesign/ZoneMusic.cs
@@ -4,29 +4,34 @@ using UnityEngine;
 
 public class ZoneMusic : ZoneAmb
 {
+    private bool hasTriggered = false;
     private void Awake()
     {
 
     }
     public override void OnZoneEnter()
     {
-        AudioSource src = AudioManager.instance.GetMusicPlayer();
-        src.loop = isLooping;
+        if (!hasTriggered)
+        {
+            hasTriggered = true;
+            AudioSource src = AudioManager.instance.GetMusicPlayer();
+            src.loop = isLooping;
 
-        if (zoneAudio == null)
-        {
-            StartCoroutine(StartFade(src, fadeOutTime, 0));
-        }
-        else
-        {
-            if (src.clip == zoneAudio && !src.isPlaying) 
+            if (zoneAudio == null)
             {
-                return;
+                StartCoroutine(StartFade(src, fadeOutTime, 0));
             }
+            else
+            {
+                if (src.clip == zoneAudio && !src.isPlaying)
+                {
+                    return;
+                }
 
-            src.clip = zoneAudio;
-            src.Play();
-            StartCoroutine(StartFade(src, fadeInTime, volume));
+                src.clip = zoneAudio;
+                src.Play();
+                StartCoroutine(StartFade(src, fadeInTime, volume));
+            }
         }
     }
     public override void OnZoneExit()
