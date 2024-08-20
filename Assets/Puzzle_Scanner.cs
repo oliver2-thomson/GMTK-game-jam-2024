dda9e060e7e5cf64444c60ddb9129f8bf90087f8
volx64 <115743584+volx64@users.noreply.github.com>
2024-08-20 14:12:17 +0100

    Added Scanner

---
+++ Assets/Puzzle_Scanner.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle_Scanner : MonoBehaviour
+{
+    [SerializeField] private TriggerEvents2DToggle[] patterns;
+    [SerializeField] private UnityEngine.Events.UnityEvent OnComplete;
+    [SerializeField] private UnityEngine.Events.UnityEvent OnCompleteOneShot;
+
+    private bool oneshotComplete = false;
+    private void Awake()
+    {
+        foreach(TriggerEvents2D trigger in patterns) 
+        {
+            trigger.OnTriggerEnter += OnTriggerEnter2D;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (ValidateAllCollisions()) 
+        {
+            if (!oneshotComplete) 
+            {
+                oneshotComplete = true;
+                OnCompleteOneShot?.Invoke();
+            }
+
+            OnComplete?.Invoke();
+        }
+    }
+
+    private bool ValidateAllCollisions() 
+    {
+        bool Success = true;
+        foreach(TriggerEvents2DToggle trigger in patterns) 
+        {
+            if (!trigger.Toggled) 
+            {
+                Success = false;
+            }
+        }
+
+        return Success;
+    }
+
+}
